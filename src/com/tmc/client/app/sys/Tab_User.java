aa339164d57c5a6df6874ef2ce917ea65c1f0058
middleshin <middleshin@middleshin-PC>
2016-12-23 17:27:25 +0900

    담당자 관리 수정
    Signed-off-by: middleshin <middleshin@middleshin-PC>

--- src/com/tmc/client/app/sys/Tab_User.java
+++ src/com/tmc/client/app/sys/Tab_User.java
@@ -1,14 +1,14 @@
 package com.tmc.client.app.sys;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import com.tmc.client.app.sys.model.CompanyModel;
-import com.tmc.client.app.sys.model.UserCompanyModel;
 import com.tmc.client.app.sys.model.UserModel;
 import com.tmc.client.app.sys.model.UserModelProperties;
-import com.tmc.client.main.LoginUser;
+import com.tmc.client.service.GridDeleteData;
+import com.tmc.client.service.GridInsertRow;
 import com.tmc.client.service.GridRetrieveData;
+import com.tmc.client.service.GridUpdateData;
 import com.tmc.client.ui.InterfaceLookupResult;
 import com.tmc.client.ui.SimpleMessage;
 import com.tmc.client.ui.builder.ComboBoxField;
@@ -56,7 +56,7 @@ public class Tab_User extends VerticalLayoutContainer implements InterfaceGridOp
    	 	}); 
 
 		searchBarBuilder.addLookupTriggerField(lookupCompanyName, "기관명", 250, 48); 
-		searchBarBuilder.addLabel(userNameField, "교원명", 150, 46, true); 
+		searchBarBuilder.addLabel(userNameField, "담당자", 150, 46, true); 
 
 		searchBarBuilder.addRetrieveButton(); 
 		searchBarBuilder.addUpdateButton();
@@ -74,8 +74,6 @@ public class Tab_User extends VerticalLayoutContainer implements InterfaceGridOp
 		
 		gridBuilder.addText(properties.ctzNo(), 80, "직원번호", new TextField()) ;		
 		gridBuilder.addText(properties.korName(), 80, "이름 ", new TextField()) ;
-		// gridBuilder.addText(properties.engName(), 120, "영문명", new TextField()) ;
-
 
 		final ComboBoxField genderComboBox = new ComboBoxField("GenderCode");  
 		genderComboBox.addCollapseHandler(new CollapseHandler(){
@@ -87,17 +85,12 @@ public class Tab_User extends VerticalLayoutContainer implements InterfaceGridOp
 		}); 
 		gridBuilder.addText(properties.genderName(), 60, "성별", genderComboBox) ;
 		gridBuilder.addDate(properties.birthday(), 100, "생년월일", new DateField()) ;
-		
 		gridBuilder.addText(properties.mainMajor(), 160, "전공과목", new TextField()) ;		
-		
 		gridBuilder.addDate(properties.startDate(), 100, "근무시작일", new DateField()) ;
 		gridBuilder.addDate(properties.closeDate(), 100, "근무종료일", new DateField()) ;
-		
 		gridBuilder.addText(properties.email(), 150, "이메일주소", new TextField()) ;
-		
 		gridBuilder.addText(properties.telNo01(), 120, "전화번호(1)", new TextField()) ;
 		gridBuilder.addText(properties.telNo02(), 120, "전화번호(2)", new TextField()) ;
-		
 		gridBuilder.addText(properties.loginId(), 100, "로그인아이디", new TextField()) ;
 		ColumnConfig<UserModel, String> password = gridBuilder.addText(properties.passwd(), 100, "패스워드", new PasswordField());
 
@@ -110,7 +103,6 @@ public class Tab_User extends VerticalLayoutContainer implements InterfaceGridOp
 
 		gridBuilder.addText(properties.zipCode(), 80, "우편번호", new TextField()) ;
 		gridBuilder.addText(properties.zipAddress(), 250, "주소", new TextField()) ;
-
 		gridBuilder.addText(properties.note(), 400, "비고", new TextField());
 
 		return gridBuilder.getGrid(); 
@@ -132,15 +124,29 @@ public class Tab_User extends VerticalLayoutContainer implements InterfaceGridOp
 	}
 	
 	@Override
-	public void update() {
+	public void update(){
+		GridUpdateData<UserModel> service = new GridUpdateData<UserModel>(); 
+		service.update(grid.getStore(), "sys.User.update"); 
 	}
 	
 	@Override
-	public void insertRow() {
+	public void insertRow(){
+		if(this.companyModel.getCompanyId() == null){
+			new SimpleMessage("기관선택", "등록하고자 하는 담당자의 기관을 먼저 선택하여 주세요"); 
+			return ; 
+		}
+		
+		GridInsertRow<UserModel> service = new GridInsertRow<UserModel>(); 
+		UserModel userModel = new UserModel();
+		userModel.setCompanyId(this.companyModel.getCompanyId());
+		service.insertRow(grid, userModel);
 	}
 	
 	@Override
-	public void deleteRow() {
+	public void deleteRow(){
+		GridDeleteData<UserModel> service = new GridDeleteData<UserModel>();
+		List<UserModel> checkedList = grid.getSelectionModel().getSelectedItems() ; 
+		service.deleteRow(grid.getStore(), checkedList, "sys.User.delete");
 	}
 
 	@Override
