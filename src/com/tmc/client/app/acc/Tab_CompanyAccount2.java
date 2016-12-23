22ef7c004ab19ef1d1898bf80f314aecc9160c35
psmis <psmis@PSmis>
2016-12-23 22:21:07 +0900

    Signed-off-by: psmis <psmis@PSmis>

--- src/com/tmc/client/app/acc/Tab_CompanyAccount2.java
+++ src/com/tmc/client/app/acc/Tab_CompanyAccount2.java
@@ -42,7 +42,7 @@ public class Tab_CompanyAccount2 extends VerticalLayoutContainer implements Inte
 	private LookupTriggerField lookupCompanyName = new LookupTriggerField() ; 
 	private CompanyModel companyModel;
 	
-	ComboBoxField eduOfficeName = new ComboBoxField("EduOfficeCode");
+	ComboBoxField eduOfficeName = new ComboBoxField("EduOfficeCode_CCCCCCCCCC");
 	
 	public Tab_CompanyAccount2() {
 		
