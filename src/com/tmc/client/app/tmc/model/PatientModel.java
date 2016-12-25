d4d22a05be44bc4a56552fa1949eea320e41d1b3
psmis <psmis@PSmis>
2016-12-25 22:34:21 +0900

    Signed-off-by: psmis <psmis@PSmis>

--- src/com/tmc/client/app/tmc/model/PatientModel.java
+++ src/com/tmc/client/app/tmc/model/PatientModel.java
@@ -21,6 +21,7 @@ public class PatientModel extends AbstractDataModel {
 	private String viewPoint;
 	private Date 	birthday;
 	private String genderCode;
+	private String genderName;
 	private String insNo;
 	private Long 	companyId;
 	private String note;
@@ -171,6 +172,14 @@ public class PatientModel extends AbstractDataModel {
 
 	public void setCompanyModel(CompanyModel companyModel) {
 		this.companyModel = companyModel;
+	}
+
+	public String getGenderName() {
+		return genderName;
+	}
+
+	public void setGenderName(String genderName) {
+		this.genderName = genderName;
 	}
 
 }
