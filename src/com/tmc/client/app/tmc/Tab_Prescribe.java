4e2d7136a6f21ab426a2637d34555e8c0b1bb07b
middleshin <middleshin@middleshin-PC>
2017-01-10 18:55:38 +0900

    Signed-off-by: middleshin <middleshin@middleshin-PC>

--- src/com/tmc/client/app/tmc/Tab_Prescribe.java
+++ src/com/tmc/client/app/tmc/Tab_Prescribe.java
@@ -1,19 +1,14 @@
 package com.tmc.client.app.tmc;
 
 import java.util.Date;
-import com.tmc.client.app.sys.Lookup_Company;
-import com.tmc.client.app.sys.model.CompanyModel;
 import com.tmc.client.app.tmc.model.RequestModel;
 import com.tmc.client.app.tmc.model.RequestModelProperties;
-import com.tmc.client.main.LoginUser;
 import com.tmc.client.service.GridRetrieveData;
 import com.tmc.client.service.InterfaceCallback;
-import com.tmc.client.ui.InterfaceLookupResult;
 import com.tmc.client.ui.SimpleMessage;
 import com.tmc.client.ui.builder.GridBuilder;
 import com.tmc.client.ui.builder.InterfaceGridOperate;
 import com.tmc.client.ui.builder.SearchBarBuilder;
-import com.tmc.client.ui.field.LookupTriggerField;
 import com.google.gwt.core.client.GWT;
 import com.sencha.gxt.core.client.Style.SelectionMode;
 import com.sencha.gxt.core.client.util.Margins;
@@ -22,47 +17,27 @@ import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer.Verti
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
 import com.sencha.gxt.widget.core.client.event.RowClickEvent;
 import com.sencha.gxt.widget.core.client.event.RowClickEvent.RowClickHandler;
-import com.sencha.gxt.widget.core.client.event.TriggerClickEvent;
-import com.sencha.gxt.widget.core.client.event.TriggerClickEvent.TriggerClickHandler;
 import com.sencha.gxt.widget.core.client.form.DateField;
 import com.sencha.gxt.widget.core.client.form.TextField;
 import com.sencha.gxt.widget.core.client.grid.Grid;
+import com.sencha.gxt.widget.core.client.info.Info;
 
 public class Tab_Prescribe extends BorderLayoutContainer implements InterfaceGridOperate {
 	
 	private RequestModelProperties properties = GWT.create(RequestModelProperties.class);
 	private Grid<RequestModel> grid = this.buildGrid();
 	private Grid<RequestModel> gridHistory = this.buildGridHistory();
-	private Page_Treat pageTreat = new Page_Treat(gridHistory); 
+	
+
+	private Page_Treat pageTreat = new Page_Treat(this); 
 	
 	private TextField patientNameField = new TextField();
 	private DateField dateField = new DateField(); 
-	private CompanyModel companyModel = LoginUser.getLoginUser().getCompanyModel(); 
+	//private CompanyModel companyModel = LoginUser.getLoginUser().getCompanyModel(); 
+	private LookupCompanyField lookupCompanyField = new LookupCompanyField();
 	
 	public Tab_Prescribe() {
 
-		final LookupTriggerField lookupCompanyField = new LookupTriggerField(); 
-		//final Lookup_Company lookupCompany = new Lookup_Company();
-		
-		lookupCompanyField.setEditable(false);
-		lookupCompanyField.addTriggerClickHandler(new TriggerClickHandler(){
-   	 		@Override
-			public void onTriggerClick(TriggerClickEvent event) {
-   	 			Lookup_Company lookupCompany = new Lookup_Company();
-   	 			lookupCompany.setCallback(new InterfaceLookupResult(){
-	   				@Override
-	   				public void setLookupResult(Object result) {
-	   					companyModel = (CompanyModel)result;
-	   					lookupCompanyField.setText(companyModel.getCompanyName());
-	   				}
-	   			});	
-   	 			lookupCompany.show();
-			}
-   	 	}); 
-
-		this.companyModel = LoginUser.getLoginUser().getCompanyModel(); 
-		lookupCompanyField.setText(companyModel.getCompanyName());
-		
 		SearchBarBuilder searchBarBuilder = new SearchBarBuilder(this);
 		
 		searchBarBuilder.addLookupTriggerField(lookupCompanyField, "진료기관", 250, 58);
@@ -100,7 +75,7 @@ public class Tab_Prescribe extends BorderLayoutContainer implements InterfaceGri
 		northLayoutData.setMaxSize(1000);
 		this.setNorthWidget(vlc, northLayoutData); 
 
-		BorderLayoutData westLayoutData = new BorderLayoutData(400);
+		BorderLayoutData westLayoutData = new BorderLayoutData(500);
 		westLayoutData.setMargins(new Margins(2,0,0,0));
 		westLayoutData.setSplit(true);
 		westLayoutData.setMaxSize(1000);
@@ -124,10 +99,11 @@ public class Tab_Prescribe extends BorderLayoutContainer implements InterfaceGri
 		
 		gridBuilder.addText(properties.korName(), 100, "보건의"); //, lookupRequestUserField);
 		gridBuilder.addText(properties.requestNote(), 200, "진료요청내용"); //, new TextField()) ;
+		gridBuilder.addText(properties.note(), 400, "특기사항"); //, new TextField()) ;
+		
 		gridBuilder.addDate(properties.treatDate(), 100, "진료일"); //, new DateField());
 		gridBuilder.addText(properties.treatKorName(), 80, "진료의"); //, lookUpTreatUserField) ;
 		gridBuilder.addText(properties.treatNote(), 200, "처방내역"); //, new TextField()) ;
-		gridBuilder.addText(properties.patientNote(), 400, "특기사항"); //, new TextField()) ;		
 		gridBuilder.addText(properties.regKorName(), 80, "등록자"); //, new TextField()) ;
 		gridBuilder.addDate(properties.regDate(), 100, "등록일"); //, new DateField()) ;
 		return gridBuilder.getGrid(); 
@@ -139,6 +115,7 @@ public class Tab_Prescribe extends BorderLayoutContainer implements InterfaceGri
 		// gridBuilder.setChecked(null);
 		gridBuilder.addText(properties.treatStateName(), 80, "상태구분"); 
 		gridBuilder.addDate(properties.requestDate(), 	100, "진료예정일");
+		gridBuilder.addText(properties.patientKorName(), 80, "환자명");
 		//gridBuilder.addDate(properties.treatDate(), 	100, "진료일"); //, new DateField());
 		gridBuilder.addText(properties.treatKorName(), 	80, "진료의"); //, lookUpTreatUserField) ;
 		gridBuilder.addText(properties.korName(), 		80, "보건의");
@@ -146,6 +123,10 @@ public class Tab_Prescribe extends BorderLayoutContainer implements InterfaceGri
 		return gridBuilder.getGrid(); 
 	}
 	
+	public Grid<RequestModel> getGridHistory() {
+		return gridHistory;
+	}
+
 	private void editRequestModel(RequestModel requestModel){
 		this.pageTreat.retrieve(requestModel);
 	}
@@ -168,10 +149,12 @@ public class Tab_Prescribe extends BorderLayoutContainer implements InterfaceGri
 	@Override
 	public void retrieve() {
 		
-		if(this.companyModel.getCompanyId() == null){
+		if(lookupCompanyField.getCompanyModel().getCompanyId() == null){
 			new SimpleMessage("기관명 확인", "조회조건의 기관명은 반드시 입력하세요. ");
 			return ; 
 		} 
+		
+		// clear data 
 		gridHistory.getStore().clear(); 
 		this.pageTreat.reset();
 		
@@ -179,14 +162,18 @@ public class Tab_Prescribe extends BorderLayoutContainer implements InterfaceGri
 		service.addCallback(new InterfaceCallback(){
 			@Override
 			public void callback() {
-				
 			}
 		});
 		
-		service.addParam("companyId", this.companyModel.getCompanyId());
+		service.addParam("companyId", lookupCompanyField.getCompanyModel().getCompanyId());
 		service.addParam("requestDate", dateField.getValue());
 		service.addParam("patientName", patientNameField.getText());
 		service.retrieve("tmc.Request.selectByCompanyId");
+	}
+	
+	public void update(RequestModel requestModel){
+		Info.display("info", "requestModel.getTreatStateName()"); 
+		this.grid.getStore().update(requestModel);
 	}
 	
 	@Override
