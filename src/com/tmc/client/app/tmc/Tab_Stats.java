2927204899530e45487450f048957b1c16a8beb8
middleshin <middleshin@middleshin-PC>
2017-01-15 18:18:48 +0900

    Signed-off-by: middleshin <middleshin@middleshin-PC>

--- src/com/tmc/client/app/tmc/Tab_Stats.java
+++ src/com/tmc/client/app/tmc/Tab_Stats.java
@@ -6,22 +6,40 @@ import com.tmc.client.service.GridRetrieveData;
 import com.tmc.client.ui.builder.GridBuilder;
 import com.tmc.client.ui.builder.InterfaceGridOperate;
 import com.tmc.client.ui.builder.SearchBarBuilder;
+
+import java.util.Calendar;
+import java.util.Date;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.datepicker.client.CalendarUtil;
 import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
-import com.sencha.gxt.widget.core.client.form.TextField;
+import com.sencha.gxt.widget.core.client.form.DateField;
 import com.sencha.gxt.widget.core.client.grid.Grid;
 
 public class Tab_Stats extends VerticalLayoutContainer implements InterfaceGridOperate {
 	
 	private StatsRetrieveModelProperties properties = GWT.create(StatsRetrieveModelProperties.class);
 	private Grid<StatsRetrieveModel> grid = this.buildGrid();
-	private TextField patientNameField = new TextField();
+	private DateField fromDateField = new DateField(); 
+	private DateField toDateField = new DateField(); 
 	
 	public Tab_Stats() {
-
-		
 		SearchBarBuilder searchBarBuilder = new SearchBarBuilder(this);
-		searchBarBuilder.addTextField(patientNameField, "환자명", 170, 48, true); 
+		searchBarBuilder.addDateField(fromDateField, "조회기간", 180, 60, true);
+		searchBarBuilder.addDateField(toDateField, "~", 120, 10, true).setLabelSeparator("");;
+		
+		Date today = new Date(); 
+		toDateField.setValue(today);
+		
+		CalendarUtil.addMonthsToDate(today, -12);
+		CalendarUtil.addDaysToDate(today, 1);
+		
+//		Calendar cal = Calendar.getInstance();
+		
+//	    cal.setTime(today);
+//	    cal.add(Calendar.YEAR, -1); // 1년을 더한다.
+	    fromDateField.setValue(today);
+		
 		searchBarBuilder.addRetrieveButton(); 
 
 		this.add(searchBarBuilder.getSearchBar(), new VerticalLayoutData(1, 48));
@@ -33,7 +51,7 @@ public class Tab_Stats extends VerticalLayoutContainer implements InterfaceGridO
 		GridBuilder<StatsRetrieveModel> gridBuilder = new GridBuilder<StatsRetrieveModel>(properties.keyId());  
 		
 		gridBuilder.addText(properties.companyName(), 150, "기관명") ;
-		gridBuilder.addLong(properties.patientCount(), 100, "환자수") ;
+		gridBuilder.addLong(properties.patientCount(), 100, "등록환자수") ;
 		gridBuilder.addLong(properties.p10Count(), 100, "진료요청") ;
 		gridBuilder.addLong(properties.p20Count(), 100, "검사요청") ;
 		gridBuilder.addLong(properties.p30Count(), 100, "검사진행") ;
@@ -53,7 +71,8 @@ public class Tab_Stats extends VerticalLayoutContainer implements InterfaceGridO
 //		} 
 //		
 		GridRetrieveData<StatsRetrieveModel> service = new GridRetrieveData<StatsRetrieveModel>(grid.getStore());
-		service.addParam("patientName", patientNameField.getValue());
+		service.addParam("fromDate", fromDateField.getValue());
+		service.addParam("toDate", toDateField.getValue());
 		service.retrieve("tmc.StatsRetrieve.selectByRetrieve");
 	}
 	
--- src/com/tmc/server/app/tmc/StatsRetrieve.java
+++ src/com/tmc/server/app/tmc/StatsRetrieve.java
@@ -1,7 +1,10 @@
 package com.tmc.server.app.tmc;
 
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -14,8 +17,14 @@ public class StatsRetrieve {
 	private String mapperName = "tmc99_retrieve"; 
 	
 	public void selectByRetrieve(SqlSession sqlSession, ServiceRequest request, ServiceResult result){
-		String patientName = request.getString("patientName");
-		List<AbstractDataModel> list = sqlSession.selectList(mapperName + ".selectByRetrieve", patientName);
+		Date fromDate = request.getDate("fromDate");
+		Date toDate = request.getDate("toDate");
+		
+		Map<String, Object> param = new HashMap<String, Object>(); 
+		param.put("fromDate", fromDate); 
+		param.put("toDate", toDate);
+		
+		List<AbstractDataModel> list = sqlSession.selectList(mapperName + ".selectByRetrieve", param);
 		result.setRetrieveResult(1, "select ok", list);
 	}
 	
